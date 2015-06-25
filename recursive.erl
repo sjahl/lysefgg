00b66c685ced0a3b35bf8db85d0049d59a3e41d0
Stephen Jahl <stephenjahl@gmail.com>
2015-06-25 07:52:01 -0400

    adding a recursive tail, with a temp accumulator to hold less things in memory

--- recursive.erl
+++ recursive.erl
@@ -1,5 +1,5 @@
 -module(recursive).
--export([fac/1, len/1]).
+-export([fac/1, len/1, tail_fac/1]).
 
 fac(0) ->
     1;
@@ -10,3 +10,10 @@ len([]) ->
     0;
 len([_|T]) ->
     1 + len(T).
+
+tail_fac(N) ->
+    tail_fac(N,1).
+tail_fac(0,Acc) ->
+    Acc;
+tail_fac(N,Acc) when N > 0 ->
+    tail_fac(N-1,N*Acc).
